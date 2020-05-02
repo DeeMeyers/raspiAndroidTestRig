46494c982ee49fea9e61b2cdf963faefee3dda5a
DeeMeyers <justdmeyers@gmail.com>
2020-05-01 23:23:52 -0400

    both ips found

--- helper.py
+++ helper.py
@@ -1,6 +1,7 @@
 import paramiko
 import nmap
 import getpass
+import socket
 
 class SSHCreds:
     def __init__(self):
@@ -10,3 +11,26 @@ class SSHCreds:
     def __str__(self): 
         return self.user, self.password
 
+def get_ip_address():
+    s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    s.connect(("8.8.8.8", 80))
+    ip = s.getsockname()[0]
+    print(ip)
+    return ip
+
+
+class GetAdresses:
+    def __init__(self):
+        nm = nmap.PortScanner()
+        self.computer = {}
+        self.raspi = {}
+        print('Getting local ip adress:')
+        self.ip = get_ip_address()
+        print('Getting local address for raspi:')
+        try:
+            self.raspi = socket.gethostbyname('raspberrypi.local')
+            print(self.raspi)
+        except: 
+            print('raspi not found using raspberrypi.local')
+
+GetAdresses()
