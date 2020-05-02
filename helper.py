6d5ee5d241f75fddfabb7f0ddae84217a83f34e9
DeeMeyers <justdmeyers@gmail.com>
2020-05-02 01:36:41 -0400

    refactor

--- helper.py
+++ helper.py
@@ -2,37 +2,27 @@ import paramiko
 import getpass
 import socket
 
-class SSHCreds:
-    def __init__(self):
-        print('Please enter the SSH credentials for your RaspberryPi')
-        self.user = input("Username: ")
-        self.password = getpass.getpass()
-
 
 def get_ip_address():
     s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
     s.connect(("8.8.8.8", 80))
     ip = s.getsockname()[0]
-    print(ip)
     return ip
 
 
 class GetAdresses:
-    def __init__(self):
-        self.computer = ''
-        self.raspi = ''
 
     def localip():
             print('Getting local ip adress:')
-            self.computer = get_ip_address()
-            print(self.computer)
-            return self.computer
-    def raspberrypi():
+            computer = get_ip_address()
+            print(computer)
+            return computer
+    def raspberrypiFind():
             print('Getting local address for raspi:')
             try:
-                self.raspi = socket.gethostbyname('raspberrypi.local')
-                print(self.raspi)
-                return self.raspi
+                pi = socket.gethostbyname('raspberrypi.local')
+                print(pi)
+                return pi
             except: 
                 print('raspi not found using raspberrypi.local')
                 exit()
@@ -43,8 +33,11 @@ class SSHClient:
         pass
 
 
-class Helper:
+class Helper:   
     def __init__(self):
-        SSHClient(SSHCreds().user, SSHCreds().password, GetAdresses.raspberrypi(), GetAdresses.localip())
+        print('Please enter the SSH credentials for your RaspberryPi')
+        self.user = input("Username: ")
+        self.password = getpass.getpass()
+        SSHClient(self.user, self.password, GetAdresses.raspberrypiFind(), GetAdresses.localip())
 
 Helper()
